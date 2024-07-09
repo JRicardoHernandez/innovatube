13e7008a1c8eb16cc6cb8efe9a3d3c5f487da82e
JRicardoHernandez <riiiicho@gmail.com>
2024-07-09 23:33:06 +0200

    Modificar url para el api del backend

--- src/app/constants/general.ts
+++ src/app/constants/general.ts
@@ -1,5 +1,5 @@
 export enum $API_BASE {
-    _api_base = "http://localhost:5000",//"https://back-innovatube.onrender.com",//"http://localhost:5000",
+    _api_base = "https://back-innovatube.onrender.com",//"http://localhost:5000",
     _api_version = "v1",
     _api_youtube = "https://www.googleapis.com/",
     _api_youtube_version = "v3"
